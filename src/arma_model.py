90c8b234e777d8daeb267ffeddf5dc6f17a63daa
AderoVick <aderovique@gmail.com>
2026-07-15 18:36:28 +0300

    Add docstrings to ARMA model functions
    Added docstrings to fit_arma and forecast_steps functions.

--- src/arma_model.py
+++ src/arma_model.py
@@ -1,10 +1,14 @@
 import pandas as pd
 from statsmodels.tsa.arima.model import ARIMA
 
+
 def fit_arma(ts: pd.Series, p: int = 1, q: int = 1):
+    """Fit an ARMA(p, q) model using ARIMA with d=0."""
     model = ARIMA(ts, order=(p, 0, q))
     return model.fit()
 
+
 def forecast_steps(res, steps: int = 7):
-    f = res.get_forecast(steps=steps)
-    return f.predicted_mean, f.conf_int()
+    """Forecast future values from the fitted ARMA model."""
+    forecast = res.get_forecast(steps=steps)
+    return forecast.predicted_mean, forecast.conf_int()
